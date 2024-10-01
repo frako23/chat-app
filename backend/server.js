b26d7220f5c0b4ad14597748b18dcf82d9fd89b2
frakoDev <frako23@gmail.com>
2024-10-01 09:45:13 -0400

    starting to add singup functionality

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,22 @@ import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.routes.js"
 import connecToMongoDB from './db/connectToMongoDB.js'
-const app = express()
 
-dotenv.config()
+const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req,res) =>{
-    //root route http://localhost:5000/
-    res.send("Hello World!!")
-})
+dotenv.config()
+
+app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth",authRoutes)
+
+
+// app.get("/", (req,res) =>{
+//     root route http://localhost:5000/
+//     res.send("Hello World!!")
+// })
+
 
 app.listen(PORT, () => {
     connecToMongoDB()
