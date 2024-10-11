8a193e0328845b4989dc69ccbe8575cb8e927d3a
frakoDev <frako23@gmail.com>
2024-10-10 20:43:13 -0400

    adds homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Login from './components/pages/login/Login'
+import SignUp from './components/pages/signup/Signup'
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <SignUp />
     </div>
   )
 }
--- frontend/src/components/pages/signup/Signup.jsx
+++ frontend/src/components/pages/signup/Signup.jsx
@@ -1,5 +1,77 @@
-function Signup() {
-  return <div>Signup</div>
+import GenderCheckbox from './GenderCheckbox'
+
+function SignUp() {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          SignUp
+          <span className="text-blue-500"> ChatApp</span>
+        </h1>
+
+        <form action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              className="w-full input input-bordered h-10"
+              placeholder="John Doe"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              className="w-full input input-bordered h-10"
+              placeholder="johndoe"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              className="w-full input input-bordered h-10"
+              placeholder="Enter Password"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="text"
+              className="w-full input input-bordered h-10"
+              placeholder="Confirm Password"
+            />
+          </div>
+
+          <GenderCheckbox />
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            Already have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-accent btn-block btn-sm mt-2">
+              SignUp
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default Signup
+export default SignUp
