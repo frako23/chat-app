a1e0ff5f78e21f34bbdbf39caa95d6a780d115a1
frakoDev <frako23@gmail.com>
2024-10-14 07:58:37 -0400

    fixing signin issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http:/localhost:3001'
+        target: 'http://localhost:5000'
       }
     }
   }
