bd05c776978cc668e81883647f27a2a7100f060c
frakoDev <frako23@gmail.com>
2024-10-18 16:42:58 -0400

    adds logout functionality

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import toast from 'react-hot-toast'
+import { useAuthContext } from '../context/AuthContext'
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false)
+  const { setAuthUser } = useAuthContext()
 
   const signup = async ({
     fullName,
@@ -37,6 +39,13 @@ const useSignup = () => {
       })
 
       const data = await res.json()
+      if (data.error) {
+        throw new Error(data.error)
+      }
+
+      localStorage.setItem('chat-user', JSON.stringify(data))
+      setAuthUser(data)
+
       console.log(data)
     } catch (error) {
       toast.error(error.message)
