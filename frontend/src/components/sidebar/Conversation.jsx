1238f64a6b9d4f6d68e4a73e6e3043a8d728a276
frakoDev <frako23@gmail.com>
2024-10-19 20:19:38 -0400

    working in frontend functionalities for the chat

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,25 +1,29 @@
-const Conversation = () => {
+import useConversations from '../../zustand/useConversation'
+
+const Conversation = ({ conversation, lastIdx }) => {
+  const { selectedConversation, setSelectedConversation } = useConversations()
+  const isSelected = selectedConversation?._id === conversation._id
+  console.log(selectedConversation)
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected ? 'bg-sky-500' : ''}`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img
-              src="https://www.pngarts.com/files/5/User-Avatar-PNG-Transparent-Image.png"
-              alt="user avatar"
-            />
+            <img src={conversation.profilePic} alt="user avatar" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">John Doe</p>
-            <span className="text-xl">😎</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
           </div>
         </div>
       </div>
-
-      <div className="divider my-0 py-0 h-1"></div>
+      {!lastIdx && <div className="divider my-0 py-0 h-1"></div>}
     </>
   )
 }
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,19 @@
+import useGetConversations from '../../hooks/useGetConversations'
 import Conversation from './Conversation'
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations()
+  console.log('Conversations:', conversations)
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation.id}
+          conversation={conversation}
+          lastIdx={idx === conversations.legnth - 1}
+        />
+      ))}
     </div>
   )
 }
