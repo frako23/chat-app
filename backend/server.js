fe09a0b5a4ad15b006c1f1374a1ec5ff9e1ad529
frakoDev <frako23@gmail.com>
2024-10-20 11:19:54 -0400

    small fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import dotenv from "dotenv"
+import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
 
 import connecToMongoDB from './db/connectToMongoDB.js'
 
@@ -13,21 +13,19 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
-app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json()) // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser())
 
-
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-app.use("/api/users",userRoutes)
-
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
 // app.get("/", (req,res) =>{
 //     root route http://localhost:5000/
 //     res.send("Hello World!!")
 // })
 
-
 app.listen(PORT, () => {
-    connecToMongoDB()
-    console.log(`Server Running on port ${PORT}`)})
+  connecToMongoDB()
+  console.log(`Server Running on port ${PORT}`)
+})
